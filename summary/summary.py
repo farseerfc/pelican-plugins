397b7becae04d3067ef73dbe7187ee4ecbf2d477
Jiachen Yang <farseerfc@gmail.com>
2014-12-12 03:41:33 +0900

    support multiple summary block merge

--- summary/summary.py
+++ summary/summary.py
@@ -79,6 +79,52 @@ def run_plugin(generators):
             for page in generator.pages:
                 extract_summary(page)
 
+    def _get_content(self):
+        content = self._content
+        if self.settings['SUMMARY_BEGIN_MARKER']:
+            content = content.replace(
+                self.settings['SUMMARY_BEGIN_MARKER'], '', 1)
+        if self.settings['SUMMARY_END_MARKER']:
+            content = content.replace(
+                self.settings['SUMMARY_END_MARKER'], '', 1)
+        return content
+    instance._get_content = types.MethodType(_get_content, instance)
+
+    # extract out our summary
+    if not hasattr(instance, '_summary') and instance._content is not None:
+        content = instance._content
+        begin_summary = -1
+        end_summary = -1
+        last_end = 0
+        summary = None
+        while last_end != -1 and last_end < len(content):
+            if instance.settings['SUMMARY_BEGIN_MARKER']:
+                begin_summary = content.find(instance.settings['SUMMARY_BEGIN_MARKER'], last_end)
+            if instance.settings['SUMMARY_END_MARKER']:
+                end_summary = content.find(instance.settings['SUMMARY_END_MARKER'], last_end)
+            if end_summary == -1:
+                last_end = -1
+            else:
+                last_end = end_summary + len(instance.settings['SUMMARY_END_MARKER'])
+            if summary is None:  # first time
+                if begin_summary != -1 or end_summary != -1:
+                    # the beginning position has to take into account the length
+                    # of the marker
+                    begin_summary = (begin_summary +
+                                    len(instance.settings['SUMMARY_BEGIN_MARKER'])
+                                    if begin_summary != -1 else 0)
+                    end_summary = end_summary if end_summary != -1 else None
+                    summary = instance._update_content(content[begin_summary:end_summary], instance._context.get('localsiteurl', ''))
+            else:
+                if begin_summary == -1 or end_summary == -1:
+                    break
+                begin_summary = (begin_summary +
+                                len(instance.settings['SUMMARY_BEGIN_MARKER'])
+                                if begin_summary != -1 else 0)
+                end_summary = end_summary if end_summary != -1 else None
+                summary += instance._update_content(content[begin_summary:end_summary], instance._context.get('localsiteurl', ''))
+        if summary is not None:
+            instance._summary = summary
 
 def register():
     signals.initialized.connect(initialized)
