8a4ede8f29170b13faacdc1086c14af299457215
Jiachen Yang <farseerfc@gmail.com>
2015-02-03 03:05:20 +0900

    add ditaa

--- plantuml/plantuml_rst.py
+++ plantuml/plantuml_rst.py
@@ -15,7 +15,7 @@ from subprocess import Popen, PIPE
 
 from docutils.nodes import image, literal_block
 from docutils.parsers.rst import Directive, directives
-from docutils import utils, nodes
+from docutils import utils
 
 from pelican import logger, signals
 
@@ -30,8 +30,8 @@ class PlantUML(Directive):
     global global_siteurl
 
     option_spec = {
-        'class' : directives.class_option,
-        'alt'   : directives.unchanged,
+        'class': directives.class_option,
+        'alt': directives.unchanged,
         'format': directives.unchanged,
     }
 
@@ -63,10 +63,10 @@ class PlantUML(Directive):
             imgext = ".svg"
             outopt = "-tsvg"
         else:
-            logger.error("Bad uml image format: "+imgformat)
+            logger.error("Bad uml image format: " + imgformat)
 
         # make a name
-        name = tf.name+imgext
+        name = tf.name + imgext
 
         alt = self.options.get('alt', 'uml diagram')
         classes = self.options.pop('class', ['uml'])
@@ -78,6 +78,83 @@ class PlantUML(Directive):
         except Exception as exc:
             error = self.state_machine.reporter.error(
                 'Failed to run plantuml: %s' % (exc, ),
+                literal_block(self.block_text, self.block_text),
+                line=self.lineno)
+            nodes.append(error)
+        else:
+            if p.returncode == 0:
+                # renaming output image using an hash code, just to not pullate
+                # output directory with a growing number of images
+                name = os.path.join(path, os.path.basename(name))
+                newname = os.path.join(path,
+                    "%08x" % (adler32(body.encode('utf8')) & 0xffffffff))+imgext
+
+                try:  # for Windows
+                    os.remove(newname)
+                except Exception as exc:
+                    logger.debug('File '+newname+' does not exist, not deleted')
+
+                os.rename(name, newname)
+                url = global_siteurl + '/uml/' + os.path.basename(newname)
+                imgnode = image(uri=url, classes=classes, alt=alt)
+                nodes.append(imgnode)
+            else:
+                error = self.state_machine.reporter.error(
+                    'Error in "%s" directive: %s' % (self.name, err),
+                    literal_block(self.block_text, self.block_text),
+                    line=self.lineno)
+                nodes.append(error)
+
+        return nodes
+
+
+class Ditaa(Directive):
+    required_arguments = 0
+    optional_arguments = 0
+    has_content = True
+
+    global global_siteurl
+
+    option_spec = {
+        'class' : directives.class_option,
+        'alt'   : directives.unchanged,
+        'format': directives.unchanged,
+    }
+
+    def run(self):
+        source = self.state_machine.input_lines.source(self.lineno - self.state_machine.input_offset - 1)
+        source_dir = os.path.dirname(os.path.abspath(source))
+        source_dir = utils.relative_path(None, source_dir)
+
+        path = os.path.abspath(os.path.join('content', 'uml'))
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        nodes = []
+
+        body = '\n'.join(self.content)
+        tf = tempfile.NamedTemporaryFile(delete=True)
+        tf.write(body.encode('utf8'))
+        tf.flush()
+
+        imgext = ".png"
+
+        # make a name
+        name = tf.name + imgext
+
+        output_path = os.path.join(path, os.path.basename(name))
+
+        alt = self.options.get('alt', 'ditaa diagram')
+        classes = self.options.pop('class', ['ditaa'])
+        cmdline = ['ditaa', tf.name, output_path]
+
+        try:
+            p = Popen(cmdline, stdout=PIPE, stderr=PIPE)
+            out, err = p.communicate()
+        except Exception as exc:
+            error = self.state_machine.reporter.error(
+                'Failed to run ditaa: %s' % (exc, ),
                 literal_block(self.block_text, self.block_text),
                 line=self.lineno)
             nodes.append(error)
@@ -118,3 +195,4 @@ def register():
     """Plugin registration."""
     signals.article_generator_context.connect(custom_url)
     directives.register_directive('uml', PlantUML)
+    directives.register_directive('ditaa', Ditaa)
