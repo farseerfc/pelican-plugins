c415bedcb8085d8f80b435cd1679f4770d724e3d
Jiachen Yang <jc-yang@ist.osaka-u.ac.jp>
2015-03-16 20:26:01 +0900

    adding youtubeku

--- youku/youku.py
+++ youku/youku.py
@@ -73,5 +73,74 @@ class Youku(Directive):
             nodes.raw('', '</div>', format='html')]
 
 
+class YouTubeKu(Directive):
+    """ Embed Youku video in posts.
+
+    VIDEO_ID is required, with / height are optional integer,
+    and align could be left / center / right.
+
+    Usage:
+    .. youtubeku:: YOUTUBE_ID YOUKU
+        :align: center
+    """
+
+    def align(argument):
+        """Conversion function for the "align" option."""
+        return directives.choice(argument, ('left', 'center', 'right'))
+
+    required_arguments = 2
+    optional_arguments = 2
+    option_spec = {
+        'align': align
+    }
+
+    final_argument_whitespace = False
+    has_content = False
+
+    def run(self):
+        tubeVideoID, kuVideoID = self.arguments[0].strip(), self.arguments[1].strip()
+        width = 420
+        height = 315
+        align = 'left'
+
+        if 'width' in self.options:
+            width = self.options['width']
+
+        if 'height' in self.options:
+            height = self.options['height']
+
+        if 'align' in self.options:
+            align = self.options['align']
+
+        tube_url = 'https://www.youtube.com/embed/{}'.format(tubeVideoID)
+        tube_div_block = '<div class="youtube embed-responsive embed-responsive-16by9" align="{}">'.format(align)
+        tube_embed_block = '<iframe src="{}" frameborder="0"></iframe>'.format(tube_url)
+
+        ku_div_block = '<div class="youku embed-responsive embed-responsive-16by9" align="{}">'.format(align)
+        ku_embed_block = '<embed src="http://player.youku.com/player.php/sid/{}/v.swf" allowFullScreen="true" quality="high" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>'.format(kuVideoID)
+
+        tab_block = """
+        <ul class="nav nav-pills">
+            <li class="active"><a href="#youtube_%s" data-toggle="tab">Youtube</a></li>
+            <li><a href="#youku_%s" data-toggle="tab">Youku</a></li>
+        </ul>
+        <div id="youtubeku" class="tab-content">
+            <div class="tab-pane fade active in" id="youtube_%s">
+                %s %s %s
+            </div>
+            <div class="tab-pane fade" id="youku_%s">
+                %s %s %s
+            </div>
+        </div>
+        """ % (tubeVideoID, kuVideoID,
+               tubeVideoID,
+               tube_div_block, tube_embed_block, '</div>',
+               kuVideoID,
+               ku_div_block, ku_embed_block, '</div>')
+
+        return [nodes.raw('', tab_block, format='html')]
+
+
 def register():
     directives.register_directive('youku', Youku)
+    directives.register_directive('youtubeku', YouTubeKu)
