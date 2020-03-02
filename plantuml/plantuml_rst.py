3550f54c843edc36d71623045d305cc81a10ed74
Jiachen YANG <farseerfc@gmail.com>
2020-03-02 14:26:27 +0900

    add tikz directive

--- plantuml/plantuml_rst.py
+++ plantuml/plantuml_rst.py
@@ -108,6 +108,88 @@ class PlantUML_rst(Directive):
         return nodes
 
 
+class Tikz(Directive):
+    """ reST directive for TikZ """
+    required_arguments = 0
+    optional_arguments = 0
+    has_content = True
+
+    global global_siteurl
+
+    option_spec = {
+        'class': directives.class_option,
+        'alt': directives.unchanged,
+        'format': directives.unchanged,
+    }
+
+    def run(self):
+
+        path = os.path.abspath(os.path.join('content', 'uml'))
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        nodes = []
+        body = '\n'.join(self.content)
+        tf = tempfile.NamedTemporaryFile(delete=True)
+        tf.write('\\documentclass{standalone}\n\\usepackage{tikz}\n\\begin{document}\n'.encode('utf-8'))
+        tf.write(body.encode('utf8'))
+        tf.write('\n\\end{document}'.encode('utf-8'))
+        tf.flush()
+
+        imgformat = self.options.get('format', 'svg')
+
+        if imgformat == 'png':
+            imgext = ".png"
+            outopt = "-tpng"
+        elif imgformat == 'svg':
+            imgext = ".svg"
+            outopt = "-tsvg"
+        else:
+            logger.error("Bad tikz image format: " + imgformat)
+
+        # make a name
+        name = tf.name + imgext
+        output_path = os.path.join(path, os.path.basename(name))
+
+        alt = self.options.get('alt', 'tikz diagram')
+        classes = self.options.pop('class', ['tikz'])
+        cmdline = ['tikz2svg', tf.name, output_path]
+        logger.debug("running: "+ ' '.join(cmdline))
+
+        try:
+            p = Popen(cmdline, stdout=PIPE, stderr=PIPE)
+            out, err = p.communicate()
+        except Exception as exc:
+            error = self.state_machine.reporter.error(
+                'Failed to run tikz: %s' % exc,
+                literal_block(self.block_text, self.block_text),
+                line=self.lineno)
+            nodes.append(error)
+        else:
+            if p.returncode == 0:
+                # renaming output image using an hash code, just to not pullate
+                # output directory with a growing number of images
+                newname = os.path.join(path,
+                    "%08x" % (adler32(body.encode('utf8')) & 0xffffffff))+imgext
+
+                try:  # for Windows
+                    os.remove(newname)
+                except Exception as exc:
+                    logger.debug('File '+newname+' does not exist, not deleted')
+
+                os.rename(output_path, newname)
+                url = global_siteurl + '/uml/' + os.path.basename(newname)
+                imgnode = image(uri=url, classes=classes, alt=alt)
+                nodes.append(imgnode)
+            else:
+                error = self.state_machine.reporter.error(
+                    'Error in "%s" directive: %s' % (self.name, err),
+                    literal_block(self.block_text, self.block_text),
+                    line=self.lineno)
+                nodes.append(error)
+        return nodes
+
 class Ditaa(Directive):
     required_arguments = 0
     optional_arguments = 0
@@ -260,6 +342,7 @@ def register():
     signals.article_generator_context.connect(custom_url)
     directives.register_directive('ditaa', Ditaa)
     directives.register_directive('uml', PlantUML_rst)
+    directives.register_directive('tikz', Tikz)
     graphviz_filters = 'dot neato twopi circo fdp sfdp patchwork osage'.split(' ')
     for f in graphviz_filters:
         directives.register_directive(f, make_graphviz(f))
